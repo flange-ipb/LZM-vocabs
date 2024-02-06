3e5a8472ec1f7c6db8334fea9ae560c687e9de0b
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2024-02-06 13:59:11 +0100

    fix capitalization

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -1,19 +1,17 @@
-from rdflib import URIRef
-
 from csv2graph.cluster import add_cluster
 from test.util import graph, same_graphs
 
 
 def test_add_same_cluster_twice():
     g = graph()
-    add_cluster(g, "42", {"de": "Label in deutsch", "en": "Label in english"})
-    add_cluster(g, "42", {"de": "Label in deutsch", "en": "Label in english"})
+    add_cluster(g, "42", {"de": "Label in Deutsch", "en": "Label in English"})
+    add_cluster(g, "42", {"de": "Label in Deutsch", "en": "Label in English"})
 
     expected = graph().parse(data="""
         @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
         @prefix tc: <http://lzfdm.nfdi.de/vocab/themencluster#> .
 
-        tc:42 rdfs:label "Label in deutsch"@de, "Label in english"@en .
+        tc:42 rdfs:label "Label in Deutsch"@de, "Label in English"@en .
     """)
 
     assert same_graphs(g, expected)
--- test/test_topic.py
+++ test/test_topic.py
@@ -4,14 +4,14 @@ from test.util import graph, same_graphs
 
 def test_add_same_topic_twice():
     g = graph()
-    add_topic(g, "1", "42", {"de": "Label in deutsch", "en": "Label in english"})
-    add_topic(g, "1", "42", {"de": "Label in deutsch", "en": "Label in english"})
+    add_topic(g, "1", "42", {"de": "Label in Deutsch", "en": "Label in English"})
+    add_topic(g, "1", "42", {"de": "Label in Deutsch", "en": "Label in English"})
 
     expected = graph().parse(data="""
         @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
         @prefix thema: <http://lzfdm.nfdi.de/vocab/thema#> .
 
-        thema:1-42 rdfs:label "Label in deutsch"@de, "Label in english"@en .
+        thema:1-42 rdfs:label "Label in Deutsch"@de, "Label in English"@en .
     """)
 
     assert same_graphs(g, expected)
