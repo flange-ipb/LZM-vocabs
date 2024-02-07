5af4f5266786e76d5ef2630d7fc9fe0516dcb49e
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2024-02-07 10:49:30 +0100

    improve cluster creation and its tests

--- csv2graph/cluster.py
+++ csv2graph/cluster.py
@@ -1,16 +1,18 @@
 from typing import Dict
 
-from rdflib import Graph, RDFS
+from rdflib import Graph, RDFS, RDF
 from rdflib.term import Node, URIRef, Literal
 
+from csv2graph.namespace import fdmontology
 from csv2graph.settings import PREFIX_CLUSTER
 
 
-def add_cluster(g: Graph, cluster_id: str, names_dict: Dict) -> Node | None:
+def add_cluster(g: Graph, cluster_id: str, names_dict: Dict) -> Node:
     if not cluster_id:
-        return
+        raise Exception("Missing cluster id")
 
     cluster_node = URIRef(PREFIX_CLUSTER + cluster_id)
+    g.add((cluster_node, RDF.type, fdmontology.Themenbereich))
     for lang, label in names_dict.items():
         g.add((cluster_node, RDFS.label, Literal(label, lang=lang)))
 
--- test/test_cluster.py
+++ test/test_cluster.py
@@ -1,3 +1,5 @@
+import pytest
+
 from csv2graph.cluster import add_cluster
 from test.util import graph, same_graphs
 
@@ -8,10 +10,17 @@ def test_add_same_cluster_twice():
     add_cluster(g, "42", {"de": "Label in Deutsch", "en": "Label in English"})
 
     expected = graph().parse(data="""
+        @prefix o: <http://lzfdm.nfdi.de/ontologie/> .
         @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
         @prefix tc: <http://lzfdm.nfdi.de/vocab/themencluster#> .
 
-        tc:42 rdfs:label "Label in Deutsch"@de, "Label in English"@en .
+        tc:42 a o:Themenbereich ;
+            rdfs:label "Label in Deutsch"@de, "Label in English"@en .
     """)
 
     assert same_graphs(g, expected)
+
+
+def test_add_cluster_raises_exception_when_missing_cluster_id():
+    with pytest.raises(Exception):
+        add_cluster(graph(), "", {"de": "Label in Deutsch", "en": "Label in English"})
