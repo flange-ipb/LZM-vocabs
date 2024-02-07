644ee64b22082b987945fb6572760ef0bb1b980a
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2024-02-07 10:21:52 +0100

    improve topic creation and its tests

--- csv2graph/topic.py
+++ csv2graph/topic.py
@@ -1,16 +1,20 @@
 from typing import Dict
 
-from rdflib import Graph, RDFS
+from rdflib import Graph, RDFS, RDF
 from rdflib.term import Node, URIRef, Literal
 
+from csv2graph.namespace import fdmontology
 from csv2graph.settings import PREFIX_TOPIC
 
 
 def add_topic(g: Graph, cluster_id: str, topic_id: str, names_dict: Dict) -> Node | None:
-    if not cluster_id or not topic_id:
-        return
+    if not cluster_id:
+        raise Exception("Missing cluster id")
+    if not topic_id:
+        raise Exception("Missing topic id")
 
     topic_node = URIRef(PREFIX_TOPIC + f"{cluster_id}-{topic_id}")
+    g.add((topic_node, RDF.type, fdmontology.Thema))
     for lang, label in names_dict.items():
         g.add((topic_node, RDFS.label, Literal(label, lang=lang)))
 
--- test/test_topic.py
+++ test/test_topic.py
@@ -1,3 +1,5 @@
+import pytest
+
 from csv2graph.topic import add_topic
 from test.util import graph, same_graphs
 
@@ -8,10 +10,22 @@ def test_add_same_topic_twice():
     add_topic(g, "1", "42", {"de": "Label in Deutsch", "en": "Label in English"})
 
     expected = graph().parse(data="""
+        @prefix o: <http://lzfdm.nfdi.de/ontologie/> .
         @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
         @prefix thema: <http://lzfdm.nfdi.de/vocab/thema#> .
 
-        thema:1-42 rdfs:label "Label in Deutsch"@de, "Label in English"@en .
+        thema:1-42 a o:Thema ;
+            rdfs:label "Label in Deutsch"@de, "Label in English"@en .
     """)
 
     assert same_graphs(g, expected)
+
+
+def test_add_topic_raises_exception_when_missing_cluster_id():
+    with pytest.raises(Exception):
+        add_topic(graph(), "", "42", {"de": "Label in Deutsch", "en": "Label in English"})
+
+
+def test_add_topic_raises_exception_when_missing_topic_id():
+    with pytest.raises(Exception):
+        add_topic(graph(), "1", "", {"de": "Label in Deutsch", "en": "Label in English"})
